c1671802d6b67fe582266831c29bac7f7783a5af
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-03-07 19:57:12 -0800

    contact test

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -27,6 +29,8 @@ describe('Nav component', () => {
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
         />);
 
         expect(asFragment()).toMatchSnapshot();
@@ -40,6 +44,8 @@ describe('emoji is visible', () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
         />);
         //assert
 
@@ -53,8 +59,10 @@ describe('links are visible', () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />);
       expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About Me');
+      expect(getByTestId('about')).toHaveTextContent('About me');
     });
   })
